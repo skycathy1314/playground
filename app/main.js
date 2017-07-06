3d01010c70284a4dd9fc0a4a69799b337707211a
Cassie.Xu <xukaiheng@shhxzq.com>
2017-07-06 17:50:04 +0800

    commit timerbar

--- app/main.js
+++ app/main.js
@@ -1,3 +1,8 @@
 /**
  * Created by Cassie.Xu on 17/7/6.
  */
+import React from 'react';
+import {render} from 'react-dom';
+import TimerBar from './components/TimerBar/TimerBar';
+
+render(<TimerBar />, document.getElementById('root'));
--- build/bundle.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Cassie.Xu on 17/7/6.
- */
